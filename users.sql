9719ebaf6cf3347d085bdbad9c64ca84075b95a0
patrus405 <34378057+patrus405@users.noreply.github.com>
2017-12-25 19:35:15 +0100

    Update users.sql

--- users.sql
+++ users.sql
@@ -1,8 +1,8 @@
 CREATE TABLE `users` (
   `id` int(11) NOT NULL,
-  `nick` text NOT NULL,
-  `email` text NOT NULL,
-  `password` text NOT NULL,
+  `nick` text CHARACTER SET utf8 COLLATE utf8_polish_ci NOT NULL,
+  `email` text CHARACTER SET utf8 COLLATE utf8_polish_ci NOT NULL,
+  `password` text CHARACTER SET utf8 COLLATE utf8_polish_ci NOT NULL,
   `date` date NOT NULL,
   `email_confirm` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
