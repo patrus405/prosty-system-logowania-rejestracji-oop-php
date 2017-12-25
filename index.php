fbb8931845ac17fa7cc87b581e1e021e1aff8180
patrus405 <34378057+patrus405@users.noreply.github.com>
2017-12-25 19:27:15 +0100

    Update index.php

--- index.php
+++ index.php
@@ -34,22 +34,6 @@
 
         </form>
 
-        <!-- <form method="post" action="registration.php">
-
-            <label for="user_nick">nick</label><br>
-                <input type="text" name="user_nick" id="user_nick" placeholder="nick"><br>
-            <label for="user_password1">hasło</label><br>
-                <input type="password" name="user_password1" id="user_password1" placeholder="hasło" ><br>
-            <label for="user_password2">powtórz hasło</label><br>
-                <input type="password" name="user_password2" id="user_password2"  placeholder="powtórz hasło"><br>
-            <label for="user_email">e-mail</label><br>
-                <input type="text" name="user_email" id="user_email" placeholder="e-mail"><br>
-            <label for="user_birthday_date">data urodzenia:</label><br>
-                <input type="date" name="user_birthday_date" id="user_birthday_date"><br>
-            <input type="submit" value="zarejstruj">
-
-        </form> -->
-
         <h1>zaloguj się:</h1>
 
         <form method='post' action="login.php">
